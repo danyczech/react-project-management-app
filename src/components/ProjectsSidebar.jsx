37983510233eeef01ff446fc4cc1df6894ae1e8f
danakal <dana.kalcikova@centrum.cz>
2024-01-04 20:54:17 +0100

    components & useState

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+const ProjectSidebar = ({ onStartAddProject }) => {
+    return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Tvoje projekty</h2>
+        <div>
+            <Button onClick={onStartAddProject}>+ Přidej projekt</Button>
+        </div>
+    </aside>
+}
+
+export default ProjectSidebar;
