10a6ee95af17b6ff50dafde3694ef4571d357d5d
danakal <dana.kalcikova@centrum.cz>
2024-01-11 19:29:54 +0100

    useRef solution

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const NewTask = ({ onAdd }) => {
+    const [enteredTask, setEnteredTask] = useState('');
+
+    const handleChange = (event) => {
+        setEnteredTask(event.target.value);
+    }
+
+    const handleClick = () => {
+        if (enteredTask.trim() === '') {
+            return;
+        }
+        onAdd(enteredTask);
+        setEnteredTask('');
+    }
+
+    return (
+        <div className="flex items-center gap-4">
+            <input
+                onChange={handleChange}
+                value={enteredTask}
+                type="text"
+                className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            <button onClick={handleClick} className="text-stone-700 hover:text-stone-950">Přidej úkol</button>
+        </div>
+    )
+}
+
+export default NewTask;
